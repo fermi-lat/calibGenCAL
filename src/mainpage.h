53f761afaafb9b1c8ad48ba3cb4e79e066ada335
xchen <>
2003-12-11 19:59:30 +0000

    codes for light taper study

--- src/mainpage.h
+++ src/mainpage.h
@@ -11,6 +11,8 @@ This package contains codes to generate and study calibration constants for CAL 
 <li> getMuSlopesXML.exe produces calibration constants for muon slope in xml format. Usage: getMuSlopesXML.exe ../output/muslopes.txt where ../output/muslopes.txt is name of ascii file containing the constants. It produces an xml file at ../xml/muslopes.xml </li>
 <li> getGainsXML.exe produces calibration constants for gain in xml format. Usage: getGainsXML.exe ../output/gains.txt where ../output/gains.txt is name of ascii file containing the constants. It produces an xml file at ../xml/pedestals.xml </li>
 <li>genNtuple.exe, compare_constants.exe utility executables used to inspect calibration constants
+<li> runLightTaperCalib.exe. It read data from a digi root file and a recon root file. The executable takes inputs from lightTaperCalib_option.dat</li>
+<li> runLightTaperCalibChain.exe. Its function is similar to the function of runLightTaperCalib.exe. The only differenece is that it can read data from a chain of digi root files and recon root files. The executable takes inputs from lightTaperCalibChain_option.dat</li>
 </ul>
 
 */
