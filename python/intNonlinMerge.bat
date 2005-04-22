52d4eeb9d80cb1ce01de1040936c433feedfa2fd
dwood <>
2005-04-22 21:26:10 +0000

    Use CMT CALIBGENCALROOT env variable to locate python scripts directory.

--- python/intNonlinMerge.bat
+++ python/intNonlinMerge.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal
-python D:\Programs\calibGenCAL\python\intNonlinMerge.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+set PYTHONROOT=%CALIBGENCALROOT%\python;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\intNonlinMerge.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
