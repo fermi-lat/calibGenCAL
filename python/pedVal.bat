d1e33503a1db3feada78993eb166fe56f2183fec
dwood <>
2005-04-25 19:07:30 +0000

    Made python scripts seach look in CALIBGENCALROOT CMT env directory.

--- python/pedVal.bat
+++ python/pedVal.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal
-python D:\Programs\calibGenCAL\python\pedVal.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+set PYTHONROOT=%CALIBGENCALROOT%\python;%ROOTSYS%\bin;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\pedVal.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
