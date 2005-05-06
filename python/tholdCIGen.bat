e8585d0a98ec8e42a19d4831411369492a072cc8
dwood <>
2005-05-06 17:45:26 +0000

    Made paths use CALIBGENCALROOT env variable.

--- python/tholdCIGen.bat
+++ python/tholdCIGen.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal
-python D:\Programs\calibGenCAL\python\tholdCIGen.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+set PYTHONROOT=%CALIBGENCALROOT%\python;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\tholdCIGen.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
