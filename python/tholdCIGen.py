10c6ae77ce9826ccfc01fb78ce066d04132c2a7a
dwood <>
2005-05-16 17:12:20 +0000

    Re-worked config file option names; allowed for single snapshot XML file instead of multiple DAC settings XML files; read config_0 settings in snapshot to obtain gain settings for each FE.

--- python/tholdCIGen.py
+++ python/tholdCIGen.py
@@ -15,231 +15,206 @@ import calCalibXML
 
 
 
-usage = "genTholdCI <cfg_file> <out_xml_file>"
+if __name__ == '__main__':
 
 
-# setup logger
-
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-
-# check command line
-
-try:
-    opts = getopt.getopt(sys.argv[1:], "")
-except getopt.GetoptError:
-    log.error(usage)
-    sys.exit(1)
-    
-args = opts[1]
-if len(args) != 2:
-    log.error(usage)
-    sys.exit(1)
-
-configName = args[0]
-calibName = args[1]
+    usage = "genTholdCI <cfg_file> <out_xml_file>"
 
 
-# read config file settings
+    # setup logger
 
-log.info("genTholdCI: Reading file %s", configName)
-configFile = ConfigParser.SafeConfigParser()
-configFile.read(configName)
-sections = configFile.sections()
-if len(sections) == 0:
-    log.error("config file %s missing or empty" % configName)
-    sys.exit(1)
+    logging.basicConfig()
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
 
-# get DAC setttings file names
+    # get environment settings
 
-if 'dacfiles' not in sections:
-    log.error("config file %s missing [dacfiles] section" % configName)
-    sys.exit(1)
-if not configFile.has_option('dacfiles', 'uldfile'):
-    log.error("config file %s missing [dacfiles]:uldfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('dacfiles', 'lacfile'):
-    log.error("config file %s missing [dacfiles]:lacfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('dacfiles', 'flefile'):
-    log.error("config file %s missing [dacfiles]:flefile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('dacfiles', 'fhefile'):
-    log.error("config file %s missing [dacfiles]:fhefile option" % configName)
-    sys.exit(1)
+    try:
+        calibUtilRoot = os.environ['CALIBUTILROOT']
+    except:
+        log.error('genTholdCI: CALIBUTILROOT must be defined')
+        sys.exit(1)    
 
-uldDacName = configFile.get('dacfiles', 'uldfile')
-lacDacName = configFile.get('dacfiles', 'lacfile')
-fleDacName = configFile.get('dacfiles', 'flefile')
-fheDacName = configFile.get('dacfiles', 'fhefile')
+    # check command line
 
-# get DAC/ADC characterization file names
+    try:
+        opts = getopt.getopt(sys.argv[1:], "")
+    except getopt.GetoptError:
+        log.error(usage)
+        sys.exit(1)
+        
+    args = opts[1]
+    if len(args) != 2:
+        log.error(usage)
+        sys.exit(1)
 
-if 'adcfiles' not in sections:
-    log.error("config file %s missing [adcfiles] section" % configName)
-    sys.exit(1)
-if not configFile.has_option('adcfiles', 'uld2adcfile'):
-    log.error("config file %s missing [adcfiles]:uld2adcfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('adcfiles', 'lac2adcfile'):
-    log.error("config file %s missing [adcfiles]:lac2adcfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('adcfiles', 'fle2adcfile'):
-    log.error("config file %s missing [adcfiles]:fle2adcfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('adcfiles', 'fhe2adcfile'):
-    log.error("config file %s missing [adcfiles]:fhe2adcfile option" % configName)
-    sys.exit(1)
-if not configFile.has_option('adcfiles', 'intnonlinfile'):
-    log.error("config file %s missing [adcfiles]:intnonlinfile option" % configName)
-    sys.exit(1)
-
-uldAdcName = configFile.get('adcfiles', 'uld2adcfile')
-lacAdcName = configFile.get('adcfiles', 'lac2adcfile')
-fleAdcName = configFile.get('adcfiles', 'fle2adcfile')
-fheAdcName = configFile.get('adcfiles', 'fhe2adcfile')
-intNonlinName = configFile.get('adcfiles', 'intnonlinfile')
-
-# get pedestal value file names
-
-if 'pedfiles' not in sections:
-    log.error("config file %s missing [pedfiles] section" % configName)
-    sys.exit(1)
-if not configFile.has_option('pedfiles', 'pedfile'):
-    log.error("config file %s missing [pedfiles]:pedfile option" % configName)
-    sys.exit(1)
-
-pedName = configFile.get('pedfiles', 'pedfile')
-
-# get DTD spec file names
-
-if 'dtdfiles' not in sections:
-    log.error("config file %s missing [dtdfiles] section" % configName)
-    sys.exit(1)
-if not configFile.has_option('dtdfiles', 'dtdfile'):
-    log.error("config file %s missing [dtdfiles]:dtdfile option" % configName)
-    sys.exit(1)
-
-dtdName = configFile.get('dtdfiles', 'dtdfile')
+    configName = args[0]
+    calibName = args[1]
 
 
-# read FLE DAC config file
+    # read config file settings
 
-log.info("genTholdCI: Reading file %s", fleDacName)
-fleDacFile = calDacXML.calDacXML(fleDacName, 'fle_dac')
-fleDacData = fleDacFile.read()
-fleDacFile.close()
+    log.info("genTholdCI: Reading file %s", configName)
+    configFile = ConfigParser.SafeConfigParser()
+    configFile.read(configName)
+    sections = configFile.sections()
+    if len(sections) == 0:
+        log.error("config file %s missing or empty" % configName)
+        sys.exit(1)
 
-# read FHE DAC config file
+    # get DAC setttings file names
 
-log.info("genTholdCI: Reading file %s", fheDacName)
-fheDacFile = calDacXML.calDacXML(fheDacName, 'fhe_dac')
-fheDacData = fheDacFile.read()
-fheDacFile.close()
+    if 'dacfiles' not in sections:
+        log.error("config file %s missing [dacfiles] section" % configName)
+        sys.exit(1)
+    if not configFile.has_option('dacfiles', 'snapshot'):
+        log.error("config file %s missing [dacfiles]:snapshot option" % configName)
+        sys.exit(1)
 
-# read LAC DAC config file
+    snapshotName = configFile.get('dacfiles', 'snapshot')
 
-log.info("genTholdCI: Reading file %s", lacDacName)
-lacDacFile = calDacXML.calDacXML(lacDacName, 'log_acpt')
-lacDacData = lacDacFile.read()
-lacDacFile.close()
 
-# read ULD DAC config file
+    # get DAC/ADC characterization file names
 
-log.info("genTholdCI: Reading file %s", uldDacName)
-uldDacFile = calDacXML.calDacXML(uldDacName, 'rng_uld_dac')
-uldDacData = uldDacFile.read()
-uldDacFile.close()
+    if 'adcfiles' not in sections:
+        log.error("config file %s missing [adcfiles] section" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'uld2adc'):
+        log.error("config file %s missing [adcfiles]:uld2adcfile option" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'lac2adc'):
+        log.error("config file %s missing [adcfiles]:lac2adcfile option" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'fle2adc'):
+        log.error("config file %s missing [adcfiles]:fle2adcfile option" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'fhe2adc'):
+        log.error("config file %s missing [adcfiles]:fhe2adcfile option" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'intnonlin'):
+        log.error("config file %s missing [adcfiles]:intnonlin option" % configName)
+        sys.exit(1)
+    if not configFile.has_option('adcfiles', 'pedestals'):
+        log.error("config file %s missing [adcfiles]:pedestals option" % configName)
+        sys.exit(1)    
 
-# read LAC/ADC characterization file
+    uldAdcName = configFile.get('adcfiles', 'uld2adc')
+    lacAdcName = configFile.get('adcfiles', 'lac2adc')
+    fleAdcName = configFile.get('adcfiles', 'fle2adc')
+    fheAdcName = configFile.get('adcfiles', 'fhe2adc')
+    intNonlinName = configFile.get('adcfiles', 'intnonlin')
+    pedName = configFile.get('adcfiles', 'pedestals')
 
-log.info("genTholdCI: Reading file %s", lacAdcName)
-lacAdcFile = calFitsXML.calFitsXML(fileName = lacAdcName, mode = calFitsXML.MODE_READONLY)
-lacAdcData = lacAdcFile.read()
-lacAdcFile.close()
 
-# read ULD/ADC characterization file
 
-log.info("genTholdCI: Reading file %s", uldAdcName)
-uldAdcFile = calFitsXML.calFitsXML(fileName = uldAdcName, mode = calFitsXML.MODE_READONLY)
-uldAdcData = uldAdcFile.read()
-uldAdcInfo = uldAdcFile.info()
-uldAdcFile.close()
+    # get DTD spec file names
 
-# read FLE/ADC characterization file
+    if 'dtdfiles' not in sections:
+        log.error("config file %s missing [dtdfiles] section" % configName)
+        sys.exit(1)
+    if not configFile.has_option('dtdfiles', 'dtdfile'):
+        log.error("config file %s missing [dtdfiles]:dtdfile option" % configName)
+        sys.exit(1)
 
-log.info("genTholdCI: Reading file %s", fleAdcName)
-fleAdcFile = calFitsXML.calFitsXML(fileName = fleAdcName, mode = calFitsXML.MODE_READONLY)
-fleAdcData = fleAdcFile.read()
-fleAdcFile.close()
+    dtdName = os.path.join(calibUtilRoot, 'xml', configFile.get('dtdfiles', 'dtdfile'))
 
-# read FHE/ADC characterization file
 
-log.info("genTholdCI: Reading file %s", fheAdcName)
-fheAdcFile = calFitsXML.calFitsXML(fileName = fheAdcName, mode = calFitsXML.MODE_READONLY)
-fheAdcData = fheAdcFile.read()
-fheAdcFile.close()
+    # read snapshot DAC config file
 
-# read ADC non-linearity characterization data
+    log.info("genTholdCI: Reading file %s", snapshotName)
+    snapshotFile = calDacXML.calSnapshotXML(snapshotName)
+    fleDacData = snapshotFile.read('fle_dac')
+    fheDacData = snapshotFile.read('fhe_dac')
+    lacDacData = snapshotFile.read('log_acpt')
+    uldDacData = snapshotFile.read('rng_uld_dac')
+    config0Data = snapshotFile.read('config_0')
+    snapshotFile.close()
 
-log.info("genTholdCI: Reading file %s", intNonlinName)
-intNonlinFile = calCalibXML.calIntNonlinCalibXML(intNonlinName)
-(intNonlinDacData, intNonlinAdcData) = intNonlinFile.read()
-intNonlinFile.close()
+    # get gain indicies
 
-# read pedestal values file
+    leGainData = (config0Data & 0x0007)
+    heGainData = ((config0Data & 0x0078) >> 3)
 
-log.info("genTholdCI: Reading file %s", pedName)
-pedFile = calFitsXML.calFitsXML(fileName = pedName, mode = calFitsXML.MODE_READONLY)
-pedData = Numeric.zeros((16, 9, 4, 8, 2, 12), Numeric.Float32)
-pedData[0,:] = pedFile.read()
-pedFile.close()
+    # read LAC/ADC characterization file
 
-# create CAL calibration output XML file
+    log.info("genTholdCI: Reading file %s", lacAdcName)
+    lacAdcFile = calFitsXML.calFitsXML(fileName = lacAdcName, mode = calFitsXML.MODE_READONLY)
+    lacAdcData = lacAdcFile.read()
+    lacAdcFile.close()
 
-lrefGain = int(uldAdcInfo['LREFGAIN'])
-hrefGain = int(uldAdcInfo['HREFGAIN'])
+    # read ULD/ADC characterization file
 
-log.info("genTholdCI: Creating file %s", calibName)
-calibFile = calCalibXML.calTholdCICalibXML(calibName, mode = calCalibXML.MODE_CREATE)
-dacData = (uldDacData, lacDacData, fleDacData, fheDacData)
-adcData = (uldAdcData, lacAdcData, fleAdcData, fheAdcData)
-calibFile.write(dacData, adcData, intNonlinAdcData[3], pedData, lrefGain, hrefGain)
-calibFile.close()
+    log.info("genTholdCI: Reading file %s", uldAdcName)
+    uldAdcFile = calFitsXML.calFitsXML(fileName = uldAdcName, mode = calFitsXML.MODE_READONLY)
+    uldAdcData = uldAdcFile.read()
+    uldAdcFile.close()
 
-# make copy of XML output file
+    # read FLE/ADC characterization file
 
-calibFile = open(calibName, 'r')
-lines = calibFile.readlines()
-calibFile.close()
+    log.info("genTholdCI: Reading file %s", fleAdcName)
+    fleAdcFile = calFitsXML.calFitsXML(fileName = fleAdcName, mode = calFitsXML.MODE_READONLY)
+    fleAdcData = fleAdcFile.read()
+    fleAdcFile.close()
 
-# fixup calibration XML file - insert DTD info
+    # read FHE/ADC characterization file
 
-dtdStr = '<!DOCTYPE calCalib ['
-dtdFile = open(dtdName, 'r')
-dtdLines = dtdFile.readlines()
-dtdFile.close()
-for l in dtdLines:
-    dtdStr += l
-dtdStr += ']>\n'
+    log.info("genTholdCI: Reading file %s", fheAdcName)
+    fheAdcFile = calFitsXML.calFitsXML(fileName = fheAdcName, mode = calFitsXML.MODE_READONLY)
+    fheAdcData = fheAdcFile.read()
+    fheAdcFile.close()
 
-calibFile = open(calibName, 'w')
-calibFile.write(lines[0])
-calibFile.write(dtdStr)
-calibFile.writelines(lines[1:])
-calibFile.close()
+    # read ADC non-linearity characterization data
 
-calibFile = calCalibXML.calTholdCICalibXML(calibName)
-info = calibFile.info()
-(adcData, uldData, pedData) = calibFile.read()
-calibFile.close()
-print info
-print adcData[0, :]
-print uldData[0, :]
-print pedData[0, :]
+    log.info("genTholdCI: Reading file %s", intNonlinName)
+    intNonlinFile = calCalibXML.calIntNonlinCalibXML(intNonlinName)
+    (intNonlinDacData, intNonlinAdcData) = intNonlinFile.read()
+    intNonlinFile.close()
 
-sys.exit(0)
+    # read pedestal values file
+
+    log.info("genTholdCI: Reading file %s", pedName)
+    pedFile = calFitsXML.calFitsXML(fileName = pedName, mode = calFitsXML.MODE_READONLY)
+    pedData = Numeric.zeros((16, 9, 4, 8, 2, 12), Numeric.Float32)
+    pedData[0,:] = pedFile.read()
+    pedFile.close()
+
+    # create CAL calibration output XML file
+
+    log.info("genTholdCI: Creating file %s", calibName)
+    calibFile = calCalibXML.calTholdCICalibXML(calibName, mode = calCalibXML.MODE_CREATE)
+    dacData = (uldDacData, lacDacData, fleDacData, fheDacData)
+    adcData = (uldAdcData, lacAdcData, fleAdcData, fheAdcData)
+    calibFile.write(dacData, adcData, intNonlinAdcData[3], pedData, leGainData, heGainData)
+    calibFile.close()
+
+    # make copy of XML output file
+
+    calibFile = open(calibName, 'r')
+    lines = calibFile.readlines()
+    calibFile.close()
+
+    # fixup calibration XML file - insert DTD info
+
+    dtdStr = '<!DOCTYPE calCalib ['
+    dtdFile = open(dtdName, 'r')
+    dtdLines = dtdFile.readlines()
+    dtdFile.close()
+    for l in dtdLines:
+        dtdStr += l
+    dtdStr += ']>\n'
+
+    calibFile = open(calibName, 'w')
+    calibFile.write(lines[0])
+    calibFile.write(dtdStr)
+    calibFile.writelines(lines[1:])
+    calibFile.close()
+
+    calibFile = calCalibXML.calTholdCICalibXML(calibName)
+    info = calibFile.info()
+    (adcData, uldData, pedData) = calibFile.read()
+    calibFile.close()
+
+    sys.exit(0)
 
 
 
