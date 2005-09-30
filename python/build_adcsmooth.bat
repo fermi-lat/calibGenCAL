736e4691c1be208d629c7ede999e37a3643f4bc4
fewtrell <>
2005-09-30 20:30:44 +0000

    adding launcher scripts

---
+++ python/build_adcsmooth.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if not defined CALIBGENCALROOT goto :ERROR
+
+setlocal
+set PYTHONROOT=%CALIBGENCALROOT%\python;%PYTHONROOT%;
+python %CALIBGENCALROOT%\python\build_adcsmooth.py %1 %2 %3 %4 %5 %6 %7 %8 %9
+endlocal
+goto :EXIT
+
+:ERROR
+echo ERROR: asymTXT2XML: CALIBGENCALROOT must be defined
+
+:EXIT
+
