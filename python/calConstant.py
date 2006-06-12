ac4926f32013fcadca700b2418272d7ef758c83d
dwood <>
2006-06-12 19:38:30 +0000

    Removed <cr> characters from source file.

--- python/calConstant.py
+++ python/calConstant.py
@@ -6,8 +6,8 @@ Global calorimeter constant values
 __facility__      = "Online"
 __abstract__      = "Global calorimeter constant values"
 __author__        = "D.L. Wood"
-__date__          = "$Date: 2005/09/09 16:37:49 $"
-__version__       = "$Revision: 1.7 $, $Author: dwood $"
+__date__          = "$Date: 2005/09/09 17:39:24 $"
+__version__       = "$Revision: 1.8 $, $Author: dwood $"
 __release__       = "$Name:  $"
 __credits__       = "NRL code 7650"
 
