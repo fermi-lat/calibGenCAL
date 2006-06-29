87d5db7453c57582037422d81ece92f04662bfac
dwood <>
2006-06-29 16:55:28 +0000

    Added limit checks to MuSlope gain values.

--- python/muSlopeVal.py
+++ python/muSlopeVal.py
@@ -2,15 +2,11 @@
 Validate CAL MuSlope calibration data in XML format.  The command
 line is:
 
-muSlopeVal [-V] [-E <err_limit>] [-W <warn_limit>] [-R <root_file>] [-L <log_file>] <xml_file>
+muSlopeVal [-V] [-R <root_file>] [-L <log_file>] <xml_file>
 
 where:
 
     -R <root_file> - output validation diagnostics in ROOT file
-    -E <err_limit> - error limit for pedestal sigma value for x8 ranges
-                    (default is 10.0; x1 ranges use this value / 5)
-    -W <warn_limit> - warning limit pedestal sigma value for x8 ranges
-                     (default is 8.0; x1 ranges use this value / 5)
     -L <log_file>  - save console output to log text file
     -V             - verbose; turn on debug output
     <xml_file> The CAL MuSlope calibration XML file to validate.    
@@ -20,10 +16,11 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Validate CAL MuSlope calibration data in XML format"
 __author__    = "D.L.Wood"
-__date__      = "$Date: 2006/06/28 15:57:15 $"
-__version__   = "$Revision: 1.4 $, $Author: dwood $"
+__date__      = "$Date: 2006/06/28 16:05:41 $"
+__version__   = "$Revision: 1.5 $, $Author: dwood $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
+
 
 
 import sys, os
@@ -35,6 +32,32 @@ import Numeric
 import calCalibXML
 import calConstant
 
+
+
+# limit values
+
+
+hex1GainHighWarnLimit = 24.0
+hex1GainLowWarnLimit  = 16.0
+hex1GainHighErrLimit  = 28.0
+hex1GainLowErrLimit   = 12.0
+
+hex8GainHighWarnLimit = 2.8
+hex8GainLowWarnLimit  = 1.8
+hex8GainHighErrLimit  = 3.0
+hex8GainLowErrLimit   = 1.6
+
+lex1GainHighWarnLimit = 0.35
+lex1GainLowWarnLimit  = 0.23
+lex1GainHighErrLimit  = 0.37
+lex1GainLowErrLimit   = 0.21
+
+lex8GainHighWarnLimit = 0.036
+lex8GainLowWarnLimit  = 0.024
+lex8GainHighErrLimit  = 0.039
+lex8GainLowErrLimit   = 0.021
+
+ 
 
 
 def rootHists(slopeData, fileName):
@@ -102,14 +125,80 @@ def rootHists(slopeData, fileName):
 def calcError(slopeData):
 
     status = 0
-
+    
+    for erng in range(calConstant.NUM_RNG):
+    
+        # get limit per energy range
+    
+        if erng == calConstant.CRNG_LEX8:
+        
+            highErrLimit = lex8GainHighErrLimit
+            lowErrLimit = lex8GainLowErrLimit
+            highWarnLimit = lex8GainHighWarnLimit
+            lowWarnLimit = lex8GainLowWarnLimit
+            
+        elif erng == calConstant.CRNG_LEX1:
+        
+            highErrLimit = lex1GainHighErrLimit
+            lowErrLimit = lex1GainLowErrLimit
+            highWarnLimit = lex1GainHighWarnLimit
+            lowWarnLimit = lex1GainLowWarnLimit
+            
+        elif erng == calConstant.CRNG_HEX8:
+        
+            highErrLimit = hex8GainHighErrLimit
+            lowErrLimit = hex8GainLowErrLimit
+            highWarnLimit = hex8GainHighWarnLimit
+            lowWarnLimit = hex8GainLowWarnLimit
+           
+        else:
+        
+            highErrLimit = hex1GainHighErrLimit
+            lowErrLimit = hex1GainLowErrLimit
+            highWarnLimit = hex1GainHighWarnLimit
+            lowWarnLimit = hex1GainLowWarnLimit 
+            
+        for tem in towers:
+            for row in range(calConstant.NUM_ROW):
+                for end in range(calConstant.NUM_END):
+                    for fe in range(calConstant.NUM_FE):
+                    
+                        gain = slopeData[tem,row,end,fe,erng,0]
+                        
+                        if gain > highWarnLimit or gain < lowWarnLimit:
+                        
+                            if gain > highErrLimit:
+                                msg = 'gain %0.3f > %0.3f for %d,%s%s,%d,%s' % \
+                                    (gain, highErrLimit, tem, calConstant.CROW[row],
+                                     calConstant.CPM[end], fe, calConstant.CRNG[erng])
+                                log.error(msg)
+                                status = 1
+                            elif gain < lowErrLimit:
+                                msg = 'gain %0.3f < %0.3f for %d,%s%s,%d,%s' % \
+                                    (gain, lowErrLimit, tem, calConstant.CROW[row],
+                                     calConstant.CPM[end], fe, calConstant.CRNG[erng])
+                                log.error(msg)
+                                status = 1
+                            
+                            elif gain > highWarnLimit:
+                                msg = 'gain %0.3f > %0.3f for %d,%s%s,%d,%s' % \
+                                    (gain, highWarnLimit, tem, calConstant.CROW[row],
+                                     calConstant.CPM[end], fe, calConstant.CRNG[erng])
+                                log.warning(msg)
+                            else:
+                                msg = 'gain %0.3f < %0.3f for %d,%s%s,%d,%s' % \
+                                    (gain, lowWarnLimit, tem, calConstant.CROW[row],
+                                     calConstant.CPM[end], fe, calConstant.CRNG[erng])
+                                log.warning(msg)                  
+                                                       
     return status
+
 
 
 
 if __name__ == '__main__':
 
-    usage = "muSlopeVal [-V] [-L <log_file>] [-E <err_limit>] [-W <warn_limit>] [-R <root_file>] <xml_file>"
+    usage = "muSlopeVal [-V] [-L <log_file>] [-R <root_file>] <xml_file>"
 
     rootOutput = False
         
@@ -132,10 +221,6 @@ if __name__ == '__main__':
         if o[0] == '-R':
             rootName = o[1]
             rootOutput = True
-        elif o[0] == '-E':
-            x8ErrLimit = float(o[1])
-        elif o[0] == '-W':
-            x8WarnLimit = float(o[1])
         elif o[0] == '-L':
             if os.path.exists(o[1]):
                 log.warning('Deleting old log file %s', o[1])
