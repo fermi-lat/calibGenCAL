d8e765ff2dc2900f82c78256a6cc2eebaaa652d0
dwood <>
2006-07-31 22:13:13 +0000

    Improved energy values for validation limits and histogram axis; added mean and stddev stats to output log.

--- python/dacVal.py
+++ python/dacVal.py
@@ -18,8 +18,8 @@ where:
 __facility__    = "Offline"
 __abstract__    = "Validate CAL DAC settings XML files."
 __author__      = "D.L.Wood"
-__date__        = "$Date: 2006/05/03 16:16:29 $"
-__version__     = "$Revision: 1.9 $, $Author: dwood $"
+__date__        = "$Date: 2006/07/03 19:28:23 $"
+__version__     = "$Revision: 1.10 $, $Author: dwood $"
 __release__     = "$Name:  $"
 __credits__     = "NRL code 7650"
 
@@ -31,21 +31,12 @@ import getopt
 import ConfigParser
 
 import Numeric
+import MLab
 
 import calFitsXML
 import calDacXML
 import calConstant
 
-
-
-# validation limits
-
-
-warnLimit   = 5.0
-errLimit    = 10.0
-    
-    
-    
 
 
 
@@ -102,6 +93,19 @@ def engVal(errData):
 
 
 
+def average(data):
+
+    return Numeric.average(data, axis = None)
+                
+
+
+def stddev(data):
+
+    return MLab.std(Numeric.ravel(data))
+     
+
+
+
 if __name__ == '__main__':
     
     usage = "dacVal [-V] [-R <root_file>] [-L <log_file>] FLE|FHE|LAC <MeV> <cfg_file> <dac_xml_file>"
@@ -151,12 +155,16 @@ if __name__ == '__main__':
 
     if dacType == 'FLE':
         type = 'fle_dac'
+        warnLimit = 5.0
+        errLimit = 10.0
     elif dacType == 'FHE':
         type = 'fhe_dac'
-        errLimit *= 10
-        warnLimit *= 10
+        warnLimit = 50.0
+        errLimit = 100.0
     elif dacType == 'LAC':
         type = 'log_acpt'
+        warnLimit = 0.5
+        errLimit = 1.0
     else:
         log.error("DAC type %s not supported", dacType)
         sys.exit(1)
@@ -439,6 +447,12 @@ if __name__ == '__main__':
         # clean up
 
         rootFile.Close()
+        
+    # do simple stats
+    
+    av = average(eng)
+    sd = stddev(eng)
+    log.info("%s MeV energy threshold average=%f, stddev=%f", dacType, av, sd)    
 
     # report results
 
