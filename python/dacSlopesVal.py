5ea20adda19310712e3bff8638d45daa603f7074
dwood <>
2006-10-05 17:59:03 +0000

    Restored FHE COARSE limits; does point a problem with the data when out of these ranges.

--- python/dacSlopesVal.py
+++ python/dacSlopesVal.py
@@ -16,8 +16,8 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Validate CAL DacSlopes calibration data in XML format"
 __author__    = "D.L.Wood"
-__date__      = "$Date: 2006/10/05 02:24:33 $"
-__version__   = "$Revision: 1.6 $, $Author: dwood $"
+__date__      = "$Date: 2006/10/05 16:19:52 $"
+__version__   = "$Revision: 1.7 $, $Author: dwood $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -71,8 +71,8 @@ fheFineSlopeErrHigh    = 78.0
 fheCoarseSlopeWarnLow  = 81.0
 fheCoarseSlopeWarnHigh = 113.0
 
-fheCoarseSlopeErrLow   = 49.0
-fheCoarseSlopeErrHigh  = 135.0
+fheCoarseSlopeErrLow   = 65.0
+fheCoarseSlopeErrHigh  = 129.0
 
 uldLex8SlopeWarnLow    = 0.67
 uldLex8SlopeWarnHigh   = 1.02
