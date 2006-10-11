b9a71fd50e8439ba4d03722123abf268b5ea9dec
dwood <>
2006-10-11 17:19:03 +0000

    Added plots of ULD DAC/energy curves.

--- python/dacSlopesPlot.py
+++ python/dacSlopesPlot.py
@@ -12,8 +12,8 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Plot CAL DacSlopes data as ROOT files"
 __author__    = "D.L.Wood"
-__date__      = "$Date: 2006/10/06 17:07:13 $"
-__version__   = "$Revision: 1.9 $, $Author: dwood $"
+__date__      = "$Date: 2006/10/06 20:42:59 $"
+__version__   = "$Revision: 1.1 $, $Author: dwood $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -44,7 +44,6 @@ def linear(a, b, x):
 
 def plotDAC(slopeData, offsetData, rangeData, dacType):
 
-    
     for tem in towers:
         for row in range(calConstant.NUM_ROW):
             for end in range(calConstant.NUM_END):
@@ -117,8 +116,78 @@ def plotDAC(slopeData, offsetData, rangeData, dacType):
                 
                 leg.Draw()
                 c.Update()    
-                c.Write()     
+                c.Write()  
+                
+                
+                
+def plotULD(uldData):
+
+                       
+    for tem in towers:
+        for row in range(calConstant.NUM_ROW):
+            for end in range(calConstant.NUM_END):
+                for fe in range(calConstant.NUM_FE):
+                
+                    # create frame 
+
+                    title = "ULD_T%d_%s%s_%d" % (tem, calConstant.CROW[row], calConstant.CPM[end], fe)
+                    cName = "c_%s" % title
+                
+                    c = ROOT.TCanvas(cName, title, -1)
+                    c.SetGridx()
+                    c.SetGridy()
+                 
+                    leg = ROOT.TLegend(0.88, 0.88, 0.99, 0.99)
                     
+                    graphs = []
+                    
+                    for erng in range(3):  
+    
+                        # generate idealized data for curves
+    
+                        slope = uldData[erng,tem,row,end,fe,0]  
+                        offset = uldData[erng,tem,row,end,fe,1]
+                        sat = uldData[erng,tem,row,end,fe,2]
+                    
+                        eng = linear(slope, offset, DAC)
+                        eng = Numeric.clip(eng, 0, sat)
+                        eng = array.array('f', Numeric.log10(eng))
+                        
+                        g = ROOT.TGraph(len(DAC) - 1, DAC, eng)
+                        g.SetLineColor(erng + 1)
+                        leg.AddEntry(g.GetHistogram(), "%s" % calConstant.CRNG[erng], 'L')
+                        c.Update()
+                        graphs.append(g) 
+                        
+                    # scale axes
+                        
+                    for g in graphs:
+                        h = g.GetHistogram()
+                        h.SetMaximum(4.5)
+                        h.SetMinimum(1)    
+                        
+                    # display plots
+
+                    h = graphs[-1].GetHistogram()
+                    h.SetTitle("%s: %s" % (title, xmlName))
+                    axis = h.GetXaxis()
+                    axis.SetTitle('DAC')
+                    axis.CenterTitle()
+                    axis = h.GetYaxis()
+                    axis.SetTitle('Energy (Log MeV)')
+                    axis.CenterTitle()
+                    h.Draw()
+
+                    for g in graphs:
+                        g.Draw('L')
+                        c.Update()
+                
+                    leg.Draw()
+                    c.Update()    
+                    c.Write()                         
+
+
+
 
 
 if __name__ == '__main__':
@@ -155,6 +224,10 @@ if __name__ == '__main__':
     ROOT.gROOT.Reset()
     rootFile = ROOT.TFile(rootName, "recreate")
     
+    # plot ULD curves
+    
+    plotULD(uldData)
+    
     # plot LAC curves
     
     plotDAC(dacData[...,0], dacData[...,1], rangeData[...,0], 'LAC') 
@@ -167,6 +240,7 @@ if __name__ == '__main__':
     
     plotDAC(dacData[...,4], dacData[...,5], rangeData[...,2], 'FHE') 
     
+   
     # clean up
 
     rootFile.Close()
