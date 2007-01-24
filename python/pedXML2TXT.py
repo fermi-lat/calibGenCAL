da0c76b88a5f5f59c16156b7a476077d87778cdb
fewtrell <>
2007-01-24 16:42:11 +0000

    bad identifier name

--- python/pedXML2TXT.py
+++ python/pedXML2TXT.py
@@ -15,8 +15,8 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Dump offline pedestal xml file to .txt file"
 __author__    = "Z. Fewtrell"
-__date__      = "$Date: 2006/08/03 13:11:03 $"
-__version__   = "$Revision: 1.3 $, $Author: fewtrell $"
+__date__      = "$Date: 2007/01/16 17:22:14 $"
+__version__   = "$Revision: 1.4 $, $Author: fewtrell $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -70,7 +70,7 @@ if __name__ == '__main__':
             for col in range(calConstant.NUM_FE):
                 for face in range(calConstant.NUM_END):
                     online_face = zachUtil.offline_face_to_online[face]
-                    for rng in range(calConstant.NUM_RANGE):
+                    for rng in range(calConstant.NUM_RNG):
                         print delim.join([str(x) for x in twr, lyr, col, face, rng,
                                           pedData[twr][row][online_face][col][rng][0],
                                           pedData[twr][row][online_face][col][rng][1]])
