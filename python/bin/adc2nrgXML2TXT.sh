08a9d517f12d45bbe8b6f9372bc9ae38ac6f8f06
cvs2git <>
2007-02-16 20:14:15 +0000

    This commit was manufactured by cvs2svn to create tag 'v4r5'.
    Sprout from master 2007-02-16 20:14:14 UTC fewtrell 'AutoUpdate'
    Delete:
        python/bin/adc2nrgTXT2XML.bat
        python/bin/adc2nrgTXT2XML.sh
        python/bin/adc2nrgVal.bat
        python/bin/adc2nrgVal.sh
        python/bin/adc2nrgXML2TXT.bat
        python/bin/adc2nrgXML2TXT.sh
        python/bin/adcplot.bat
        python/bin/adcplot.sh
        python/bin/adcsmooth.bat
        python/bin/adcsmooth.sh
        python/bin/asymApplyCalibGain.bat
        python/bin/asymApplyCalibGain.sh
        python/bin/asymDiff.bat
        python/bin/asymDiff.sh
        python/bin/asymMerge.bat
        python/bin/asymMerge.sh
        python/bin/asymTXT2XML.bat
        python/bin/asymTXT2XML.sh
        python/bin/asymVal.bat
        python/bin/asymVal.sh
        python/bin/asymXML2TXT.bat
        python/bin/asymXML2TXT.sh
        python/bin/biasTXT2XML.bat
        python/bin/biasTXT2XML.sh
        python/bin/biasVal.bat
        python/bin/biasVal.sh
        python/bin/build_adcsmooth.bat
        python/bin/build_adcsmooth.sh
        python/bin/calCalibApplyRelgain.bat
        python/bin/calCalibApplyRelgain.sh
        python/bin/calFitsMerge.bat
        python/bin/calFitsMerge.sh
        python/bin/calibGainCoeff.bat
        python/bin/calibGainCoeff.sh
        python/bin/charVal.bat
        python/bin/charVal.sh
        python/bin/charplot.bat
        python/bin/charplot.sh
        python/bin/checkXML.bat
        python/bin/checkXML.sh
        python/bin/dacDiff.bat
        python/bin/dacDiff.sh
        python/bin/dacSlopesGen.bat
        python/bin/dacSlopesGen.sh
        python/bin/dacSlopesPlot.bat
        python/bin/dacSlopesPlot.sh
        python/bin/dacSlopesVal.bat
        python/bin/dacSlopesVal.sh
        python/bin/dacXML2TXT.bat
        python/bin/dacXML2TXT.sh
        python/bin/dumpROOTPlots.bat
        python/bin/dumpROOTPlots.sh
        python/bin/genADC2NRG.bat
        python/bin/genADC2NRG.sh
        python/bin/genDACsettings.bat
        python/bin/genDACsettings.sh
        python/bin/genFlightGain.sh
        python/bin/genFlightPed.bat
        python/bin/genFlightPed.sh
        python/bin/inl2tuple.bat
        python/bin/inl2tuple.sh
        python/bin/inlDiff.bat
        python/bin/inlDiff.sh
        python/bin/inlPedSubtract.bat
        python/bin/inlPedSubtract.sh
        python/bin/inlTXT2XML.bat
        python/bin/inlTXT2XML.sh
        python/bin/inlXML2TXT.bat
        python/bin/inlXML2TXT.sh
        python/bin/intNonlinMerge.bat
        python/bin/intNonlinMerge.sh
        python/bin/intNonlinVal.bat
        python/bin/intNonlinVal.sh
        python/bin/linearDAC.bat
        python/bin/linearDAC.sh
        python/bin/mevPerDacMerge.bat
        python/bin/mevPerDacMerge.sh
        python/bin/mevPerDacVal.bat
        python/bin/mevPerDacVal.sh
        python/bin/mpdApplyCalibGain.bat
        python/bin/mpdApplyCalibGain.sh
        python/bin/mpdDiff.bat
        python/bin/mpdDiff.sh
        python/bin/mpdTXT2XML.bat
        python/bin/mpdTXT2XML.sh
        python/bin/mpdXML2TXT.bat
        python/bin/mpdXML2TXT.sh
        python/bin/muSlopeDiff.bat
        python/bin/muSlopeDiff.sh
        python/bin/muSlopeTXT2XML.bat
        python/bin/muSlopeTXT2XML.sh
        python/bin/muSlopeVal.bat
        python/bin/muSlopeVal.sh
        python/bin/pedDiff.bat
        python/bin/pedDiff.sh
        python/bin/pedMerge.bat
        python/bin/pedMerge.sh
        python/bin/pedTXT2XML.bat
        python/bin/pedTXT2XML.sh
        python/bin/pedVal.bat
        python/bin/pedVal.sh
        python/bin/pedXML2TXT.bat
        python/bin/pedXML2TXT.sh
        python/bin/tholdCIGen.bat
        python/bin/tholdCIGen.sh
        python/bin/tholdCIVal.bat
        python/bin/tholdCIVal.sh
        python/bin/tholdCIXML2TXT.bat
        python/bin/tholdCIXML2TXT.sh
        python/bin/txt2tuple.bat
        python/bin/txt2tuple.sh
        python/bin/valDACsettings.bat
        python/bin/valDACsettings.sh
        python/bin/xdiodeXtalk.bat
        python/bin/xdiodeXtalk.sh
        src/unit_tests/testCfgMgr.cxx

--- python/bin/adc2nrgXML2TXT.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/adc2nrgXML2TXT.sh,v 1.2 2007/02/16 18:11:48 fewtrell Exp $
-export -n DISPLAY
-
-
-python ${CALIBGENCALROOT}/python/adc2nrgXML2TXT.py "$@"
-
--- python/bin/inlTXT2XML.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/inlTXT2XML.sh,v 1.3 2007/02/16 18:11:48 fewtrell Exp $
-export -n DISPLAY
-
-
-python ${CALIBGENCALROOT}/python/inlTXT2XML.py "$@"
-
