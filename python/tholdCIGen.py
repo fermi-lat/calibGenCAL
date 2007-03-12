41e0bb849fb9958e6a79d360101c33861bb0a274
dwood <>
2007-03-12 20:52:25 +0000

    Complete re-worked tholdCIGen application so that the information is derived from a combination of DacSlopes and MuSlopes files; the raw ADC/DAC characterization tables are no longer needed.

--- python/tholdCIGen.py
+++ python/tholdCIGen.py
@@ -13,8 +13,8 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Tool to produce CAL TholdCI XML calibration data files"
 __author__    = "D.L.Wood"
-__date__      = "$Date: 2006/07/28 01:42:57 $"
-__version__   = "$Revision: 1.27 $, $Author: dwood $"
+__date__      = "$Date: 2006/09/28 15:19:54 $"
+__version__   = "$Revision: 1.28 $, $Author: dwood $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -58,6 +58,37 @@ class inputFile:
 	
 
 
+def setRange(dacs, rangeData):
+    """
+    Adjust DAC values based on FINE/COARSE range.
+    """
+    
+    return Numeric.where(rangeData, (dacs - 64), dacs)
+
+
+def linear(c1, c0):
+
+    adc = Numeric.zeros((calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
+            calConstant.NUM_FE, 64), Numeric.Float32)
+            
+    for dac in range(64):
+        adc[...,dac] = ((c1 * dac) + c0).astype(Numeric.Float32)
+        
+    return adc
+    
+    
+def linearULD(c1, c0):
+
+    adc = Numeric.zeros((3, calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
+            calConstant.NUM_FE, 64), Numeric.Float32)
+            
+    for dac in range(64):
+        adc[...,dac] = ((c1 * dac) + c0).astype(Numeric.Float32)
+        
+    return adc
+                   
+
+
 
 ##################################################################################        
 
@@ -67,7 +98,6 @@ if __name__ == '__main__':
 
 
     usage = "tholdCIGen [-V] <cfg_file> <out_xml_file>"
-
 
     # setup logger
 
@@ -79,7 +109,7 @@ if __name__ == '__main__':
 
     try:
         calibUtilRoot = os.environ['CALIBUTILROOT']
-    except:
+    except KeyError:
         log.error('CALIBUTILROOT must be defined')
         sys.exit(1)    
 
@@ -199,6 +229,11 @@ if __name__ == '__main__':
         
             log.debug('Adding %s file %s to input as tower %d (from %d)', optList[0], name,
                   destTwr, srcTwr)
+                  
+    if 'dacslope' not in options:
+        log.error("Config file %s is missing [dacfiles]:dacslope options", configName)
+        sys.exit(1)
+    dacSlopeName = configFile.get('dacfiles', 'dacslope')
 
     # get DAC/ADC characterization file names
 
@@ -206,10 +241,6 @@ if __name__ == '__main__':
         log.error("Config file %s missing [adcfiles] section" % configName)
         sys.exit(1)
 
-    uldAdcFiles = []
-    lacAdcFiles = []
-    fleAdcFiles = []
-    fheAdcFiles = []
     pedAdcFiles = []
 
     options = configFile.options('adcfiles')
@@ -218,16 +249,7 @@ if __name__ == '__main__':
         optList = opt.split('_')
         if len(optList) != 2:
             continue
-        
-        if optList[0] == 'uld2adc':
-            fList = uldAdcFiles
-        elif optList[0] == 'lac2adc':
-            fList = lacAdcFiles
-        elif optList[0] == 'fle2adc':
-            fList = fleAdcFiles
-        elif optList[0] == 'fhe2adc':
-            fList = fheAdcFiles
-        elif optList[0] == 'pedestals':
+        if optList[0] == 'pedestals':
             fList = pedAdcFiles
         else:
             continue
@@ -265,6 +287,10 @@ if __name__ == '__main__':
         sys.exit(1)
     intNonlinName = configFile.get('adcfiles', 'intnonlin')
 
+    if 'muslope' not in options:
+        log.error("Config file %s missing [adcfiles]:muslope option" % configName)
+        sys.exit(1)
+    muSlopeName = configFile.get('adcfiles', 'muslope')
 
     # get DTD spec file names
 
@@ -384,7 +410,14 @@ if __name__ == '__main__':
                 sys.exit(1)
             dacData = fheDacFile.read()
             fheDacData[f.destTwr,...] = dacData[f.srcTwr,...]
-            fheDacFile.close()        
+            fheDacFile.close() 
+            
+    # read DAC/Energy conversion file
+    
+    log.info("Reading file %s", dacSlopeName)
+    dacSlopeFile = calCalibXML.calDacSlopesCalibXML(dacSlopeName)
+    dacSlopeData = dacSlopeFile.read()
+    dacSlopeFile.close()       
 
     # get gain indicies
 
@@ -399,101 +432,8 @@ if __name__ == '__main__':
 
     pedData = Numeric.zeros((calConstant.NUM_TEM, 9, calConstant.NUM_RNG, calConstant.NUM_ROW,
                              calConstant.NUM_END, calConstant.NUM_FE), Numeric.Float32)
-    uldAdcData = Numeric.zeros((3, calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
-                                calConstant.NUM_FE, 128), Numeric.Float32)
-    lacAdcData = Numeric.zeros((calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
-                                calConstant.NUM_FE, 128), Numeric.Float32)
-    fleAdcData = Numeric.zeros((calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
-                                calConstant.NUM_FE, 128), Numeric.Float32)
-    fheAdcData = Numeric.zeros((calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
-                                calConstant.NUM_FE, 128), Numeric.Float32)
 
-    # read LAC/ADC characterization file
-    
-    for f in lacAdcFiles:    
-
-        log.info("Reading file %s", f.name)
-        lacAdcFile = calFitsXML.calFitsXML(fileName = f.name, mode = calFitsXML.MODE_READONLY)
-        i = lacAdcFile.info()
-        if i['TTYPE1'] != 'log_acpt':
-            log.error("File %s not a LAC ADC file", f.name)
-            sys.exit(1)
-        if i['ERNG'] != 'LEX8':
-            log.error("Only LEX8 energy range is supported for LAC DAC")
-            sys.exit(1)
-        twrs = lacAdcFile.getTowers()
-        if f.srcTwr not in twrs:
-            log.error("Src twr %d data not found in file %s", f.srcTwr, f.name)
-            sys.exit(1)
-        adcData = lacAdcFile.read()
-        lacAdcData[f.destTwr,...] = adcData[f.srcTwr,...]
-        lacAdcFile.close()
-
-    # read ULD/ADC characterization files
-
-    for f in uldAdcFiles:        
-    
-        log.info("Reading file %s", f.name)
-        uldAdcFile = calFitsXML.calFitsXML(fileName = f.name, mode = calFitsXML.MODE_READONLY)
-        i = uldAdcFile.info()
-        if i['TTYPE1'] != 'rng_uld_dac':
-            log.error("File %s not a ULD ADC file", f.name)
-            sys.exit(1)
-        twrs = uldAdcFile.getTowers()
-        if f.srcTwr not in twrs:
-            log.error("Src twr %d data not found in file %s", f.srcTwr, f.name)
-            sys.exit(1)
-        adcData = uldAdcFile.read()
-        f.version = uldAdcFile.getVersion()
-        uldAdcData[:,f.destTwr,...] = adcData[:,f.srcTwr,...]
-        uldAdcFile.close()
-
-    # read FLE/ADC characterization files
-
-    for f in fleAdcFiles:
-        
-        log.info("Reading file %s", f.name)
-        fleAdcFile = calFitsXML.calFitsXML(fileName = f.name, mode = calFitsXML.MODE_READONLY)
-        i = fleAdcFile.info()
-        if i['TTYPE1'] != 'fle_dac':
-            log.error("File %s not a FLE ADC file", f.name)
-            sys.exit(1)
-        erng = i['ERNG']    
-        if erng != 'LEX8' and erng != 'LEX1':
-            log.error("Only LEX8 and LEX1 energy ranges supported for FLE DAC")
-            sys.exit(1)
-        twrs = fleAdcFile.getTowers()
-        if f.srcTwr not in twrs:
-            log.error("Src twr %d data not found in file %s", f.srcTwr, f.name)
-            sys.exit(1)
-        adcData = fleAdcFile.read()
-        if erng == 'LEX1':
-	        adcData = adcData * 9.0
-        fleAdcData[f.destTwr,...] = adcData[f.srcTwr,...].astype(Numeric.Float32)
-        fleAdcFile.close()
-        
-    # read FHE/ADC characterization files
-
-    for f in fheAdcFiles:    
-
-        log.info("Reading file %s", f.name)
-        fheAdcFile = calFitsXML.calFitsXML(fileName = f.name, mode = calFitsXML.MODE_READONLY)
-        i = fheAdcFile.info()
-        if i['TTYPE1'] != 'fhe_dac':
-            log.error("File %s not a FHE ADC file", f.name)
-            sys.exit(1)
-        if i['ERNG'] != 'HEX8':
-            log.error("Only HEX8 energy range is supported for FHE DAC")
-            sys.exit(1)
-        twrs = fheAdcFile.getTowers()
-        if f.srcTwr not in twrs:
-            log.error("Src twr %d data not found in file %s", f.srcTwr, f.name)
-            sys.exit(1)
-        adcData = fheAdcFile.read()
-        fheAdcData[f.destTwr,...] = adcData[f.srcTwr,...]
-        fheAdcFile.close()
-
-    # read pedestal values file
+    # read pedestal values files
 
     for f in pedAdcFiles:    
 
@@ -508,7 +448,7 @@ if __name__ == '__main__':
             log.error("Src twr %d data not found in file %s", f.srcTwr, f.name)
             sys.exit(1)
         adcData = pedFile.read()
-        pedData[f.destTwr,...] = adcData
+        pedData[f.destTwr,...] = adcData[:]
         pedFile.close()
 
     # read bias correction file
@@ -525,70 +465,91 @@ if __name__ == '__main__':
     intNonlinFile = calCalibXML.calIntNonlinCalibXML(intNonlinName)
     (intNonlinLengthData, intNonlinDacData, intNonlinAdcData) = intNonlinFile.read()
     intNonlinFile.close()
-
-    # do pedestal corrections for ULD ADC data
-
-    for f in uldAdcFiles:
+    
+    # read muSlope ADC/Energy conversion file
+    
+    log.info("Reading file %s", muSlopeName)
+    muSlopeFile = calCalibXML.calMuSlopeCalibXML(muSlopeName)
+    muSlopeData = muSlopeFile.read()
+    muSlopeFile.close()
+    
+    # adjust DAC settings to account for range
+    
+    drng = dacSlopeData[2][...,2]
+    fheDacData = setRange(fheDacData, drng)
+    
+    drng = dacSlopeData[2][...,1]
+    fleDacData = setRange(fleDacData, drng)
+    
+    drng = dacSlopeData[2][...,0]
+    lacDacData = setRange(lacDacData, drng)
+    
+    uldDacData = (uldDacData - 64)
+    
+    # convert FHE thresholds from MeV to ADC counts
+    
+    c1 = dacSlopeData[0][...,4]
+    c0 = dacSlopeData[0][...,5]
+    eng = linear(c1, c0)
+    gain = muSlopeData[...,calConstant.CRNG_HEX8,0]
+    bias = biasAdcData[...,1]
+    fheAdcData = (eng / gain[...,Numeric.NewAxis]) - bias[...,Numeric.NewAxis]
+    fheAdcData = Numeric.clip(fheAdcData, 0, 4095)
+    
+    # convert FLE thresholds from MeV to ADC counts
+    
+    c1 = dacSlopeData[0][...,2]
+    c0 = dacSlopeData[0][...,3]
+    eng = linear(c1, c0)
+    gain = muSlopeData[...,calConstant.CRNG_LEX1,0]
+    bias = biasAdcData[...,0]
+    fleAdcData = (eng / gain[...,Numeric.NewAxis]) - bias[...,Numeric.NewAxis]
+    fleAdcData = Numeric.clip(fleAdcData, 0, 4095)
+    
+    # convert LAC thresholds from MeV to ADC counts
+    
+    c1 = dacSlopeData[0][...,0]
+    c0 = dacSlopeData[0][...,1]
+    eng = linear(c1, c0)
+    gain = muSlopeData[...,calConstant.CRNG_LEX8,0]
+    lacAdcData = (eng / gain[...,Numeric.NewAxis])
+    lacAdcData = Numeric.clip(lacAdcData, 0, 4095)
+    
+    # convert ULD thresholds from MeV to ADC counts
+    
+    c1 = dacSlopeData[1][...,0]
+    c0 = dacSlopeData[1][...,1]
+    eng = linearULD(c1, c0)
+    
+    uldAdcData = Numeric.zeros((3, calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
+        calConstant.NUM_FE, 64), Numeric.Float32)
+    
+    gain = muSlopeData[...,calConstant.CRNG_LEX8,0]
+    uldAdcData[calConstant.CRNG_LEX8,...] = (eng[calConstant.CRNG_LEX8,...] / gain[...,Numeric.NewAxis])
+    satValue = (dacSlopeData[1][calConstant.CRNG_LEX8,...,2] / gain)
+    satValue = Numeric.reshape(Numeric.repeat(satValue, 64, -1), (calConstant.NUM_TEM, 
+        calConstant.NUM_ROW, calConstant.NUM_END, calConstant.NUM_FE, 64))
+    satMask = (uldAdcData[calConstant.CRNG_LEX8,...] > satValue)
+    uldAdcData[calConstant.CRNG_LEX8,...] = Numeric.where(satMask, satValue, 
+        uldAdcData[calConstant.CRNG_LEX8,...]) 
+    
+    gain = muSlopeData[...,calConstant.CRNG_LEX1,0]
+    uldAdcData[calConstant.CRNG_LEX1,...] = (eng[calConstant.CRNG_LEX1,...] / gain[...,Numeric.NewAxis])
+    satValue = (dacSlopeData[1][calConstant.CRNG_LEX1,...,2] / gain)
+    satValue = Numeric.reshape(Numeric.repeat(satValue, 64, -1), (calConstant.NUM_TEM, 
+        calConstant.NUM_ROW, calConstant.NUM_END, calConstant.NUM_FE, 64))
+    satMask = (uldAdcData[calConstant.CRNG_LEX1,...] > satValue)
+    uldAdcData[calConstant.CRNG_LEX1,...] = Numeric.where(satMask, satValue, 
+        uldAdcData[calConstant.CRNG_LEX1,...])
         
-        # first check for ULD values that are not pedestal subtracted
-
-        if uldAdcData[0, f.destTwr, 0, 0, 0, -1] == 4095.0:
-            log.debug("Implementing pedestal subtraction for file %s, ver %d",
-                      f.name, f.version)
-            for row in range(calConstant.NUM_ROW):
-                for end in range(calConstant.NUM_END):
-                    for fe in range(calConstant.NUM_FE):
-                        for erng in range(3):
-                            if erng < calConstant.CRNG_HEX8:
-                                gData = leGain
-                            else:
-                                gData = heGain - 8
-                                if gData < 0:
-                                   gData = 8
-                            psData = pedData[f.destTwr, gData, erng, row, end, fe]
-                            uldAdcData[erng, f.destTwr, row, end, fe, :] -= psData
-
-        # correct pedestal subtraction for v1 ULD XML files
-     
-        elif f.version <= 1:
-            log.debug("Correcting pedestal subtraction for file %s, ver %d",
-                      f.name, f.version)
-            for row in range(calConstant.NUM_ROW):
-                for end in range(calConstant.NUM_END):
-                    for fe in range(calConstant.NUM_FE):
-                        for erng in range(3):
-                            if erng < calConstant.CRNG_HEX8:
-                                gData = leGain
-                            else:
-                                gData = heGain - 8
-                                if gData < 0:
-                                    gData = 8
-                            psData = pedData[f.destTwr, gData, erng, row, end, fe]
-                            paData = pedData[f.destTwr, gData, (erng + 1), row, end, fe]
-                            uldAdcData[erng, f.destTwr, row, end, fe, :] += paData
-                            uldAdcData[erng, f.destTwr, row, end, fe, :] -= psData
-
-
-    # do warning check of intNonlin saturation against ULD saturation
-
-    for f in uldAdcFiles:
-
-        for row in range(calConstant.NUM_ROW):
-            for end in range(calConstant.NUM_END):
-                for fe in range(calConstant.NUM_FE):
-                    for erng in range(3):
-
-                        intData = intNonlinAdcData[erng]
-                        lenData = intNonlinLengthData[erng]
-                        dLen = int(lenData[f.destTwr, row, end, fe, 0])
-                        id = intData[f.destTwr, row, end, fe, (dLen - 1)]
-                        dac = int(uldDacData[f.destTwr, row, end, fe])
-                        uld = uldAdcData[erng, f.destTwr, row, end, fe, dac]
-                        if uld > id:
-                            log.warning("ULD %0.3f > INTNONLIN %0.3f for T%d,%s%s,%d,%s", uld, id, f.destTwr,
-                                calConstant.CROW[row], calConstant.CPM[end], fe, calConstant.CRNG[erng])
-                
-                               
+    gain = muSlopeData[...,calConstant.CRNG_HEX8,0]
+    uldAdcData[calConstant.CRNG_HEX8,...] = (eng[calConstant.CRNG_HEX8,...] / gain[...,Numeric.NewAxis])
+    satValue = (dacSlopeData[1][calConstant.CRNG_HEX8,...,2] / gain)
+    satValue = Numeric.reshape(Numeric.repeat(satValue, 64, -1), (calConstant.NUM_TEM, 
+        calConstant.NUM_ROW, calConstant.NUM_END, calConstant.NUM_FE, 64))
+    satMask = (uldAdcData[calConstant.CRNG_HEX8,...] > satValue)
+    uldAdcData[calConstant.CRNG_HEX8,...] = Numeric.where(satMask, satValue, 
+        uldAdcData[calConstant.CRNG_HEX8,...])
 
     # create CAL calibration output XML file
 
@@ -612,24 +573,16 @@ if __name__ == '__main__':
     for f in lacDacFiles:
         c = doc.createComment("Input LAC DAC settings file = %s" % os.path.basename(f.name)) 
         doc.appendChild(c) 
-    for f in uldAdcFiles:
-        c = doc.createComment("Input ULD ADC characterization file = %s" % os.path.basename(f.name))
-        doc.appendChild(c)
-    for f in fleAdcFiles:
-        c = doc.createComment("Input FLE ADC characterization file = %s" % os.path.basename(f.name))
-        doc.appendChild(c)
-    for f in fheAdcFiles:
-        c = doc.createComment("Input FHE ADC characterization file = %s" % os.path.basename(f.name))
-        doc.appendChild(c)            
-    for f in lacAdcFiles:
-        c = doc.createComment("Input LAC ADC characterization file = %s" % os.path.basename(f.name))
-        doc.appendChild(c)
     for f in pedAdcFiles:
         c = doc.createComment("Input pedestal value file = %s" % os.path.basename(f.name))
         doc.appendChild(c) 
     c = doc.createComment("Input bias value file = %s" % os.path.basename(biasName))
     doc.appendChild(c)     
     c = doc.createComment("Input IntNonlin file = %s" % os.path.basename(intNonlinName))
+    doc.appendChild(c)
+    c = doc.createComment("Input DacSlope file = %s" % os.path.basename(dacSlopeName))
+    doc.appendChild(c)
+    c = doc.createComment("Input MuSlope file = %s" % os.path.basename(muSlopeName))
     doc.appendChild(c)                         
     
     dacData = (uldDacData, lacDacData, fleDacData, fheDacData)
