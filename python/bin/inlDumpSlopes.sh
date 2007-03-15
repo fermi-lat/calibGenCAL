ed11482dd3f7c00c49270b6462f394120e0bff8c
fewtrell <>
2007-03-15 14:34:58 +0000

    dumps linear slopes for each intNonlin channel

---
+++ python/bin/inlDumpSlopes.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/bin/inlDumpSlopes.sh,v 1.1 2007/02/16 18:57:35 dwood Exp $
+export -n DISPLAY
+
+
+python ${CALIBGENCALROOT}/python/inlDumpSlopes.py "$@"
+
