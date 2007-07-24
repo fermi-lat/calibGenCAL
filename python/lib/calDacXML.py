714b915a3b1b7724044069c906dc71c13f87459f
dwood <>
2007-07-24 17:55:55 +0000

    Added class calSettingsXML to handle slight variation on DAC settings files produced by ConfigSystem online tool.

--- python/lib/calDacXML.py
+++ python/lib/calDacXML.py
@@ -6,8 +6,8 @@ Classes to represent CAL hardware settings XML documents.
 __facility__  = "Offline"
 __abstract__  = "Classes to represent CAL DAC settings XML documents"
 __author__    = "D.L.Wood"
-__date__      = "$Date: 2006/07/27 19:25:32 $"
-__version__   = "$Revision: 1.5 $, $Author: dwood $"
+__date__      = "$Date: 2007/03/21 20:10:31 $"
+__version__   = "$Revision: 1.6 $, $Author: dwood $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -569,6 +569,210 @@ class calEnergyXML(calXML.calXML):
 
         self.writeFile()
 
+
+
+class calSettingsXML(calXML.calXML):
+    """
+    CAL ConfigSystem settings XML data file class.
+    
+    This class provides methods for accessing CAL HW configuration
+    data stored in XML file format produced for use with the ConfigSystem
+    online tool.
+    """
+
+    def __init__(self, fileName, dacName, mode = MODE_READONLY):
+        """
+        Open a CAL configuration XML file
+
+        Param: fileName - The XML file name.
+        Param: mode - The file access mode (MODE_READONLY or MODE_CREATE).
+        Param: dacName - The name of the DAC element.
+        
+        The following DAC types are supported:
+
+            fle_dac - CAL FLE DAC setttings
+            fhe_dac - CAL FHE DAC settings
+            log_acpt - CAL LAC DAC settings
+            rng_uld_dac - CAL ULD DAC settings
+        """
+        
+        if dacName not in DAC_FILE_TYPES:
+            raise calFileOpenExcept, "DAC name %s not supported" % str(dacName)
+        self.__dacName = dacName
+        
+        calXML.calXML.__init__(self, fileName, mode)       
+
+
+    def getTowers(self):
+        """
+        Get the ID's of towers contributing to the data file.
+
+        Returns: A list of tower ID's.        
+        """
+
+        towers = []
+
+        # find <GTEM> elements
+
+        doc = self.getDoc()
+
+        latList = doc.xpath('.//LATC_XML')
+        latLen = len(latList)
+        if latLen != 1:
+            raise calFileReadExcept, "found %d <LATC_XML> elements, expected 1" % latLen
+        temList = latList[0].xpath('.//TEM')
+        temLen = len(temList)
+        if temLen > 16:
+            raise calFileReadExcept, "found %d <TEM> elements, expected <= 16" % temLen
+        for t in temList:
+            tem = int(t.getAttributeNS(None, 'ID'))
+            if tem < 0 or tem > 16:
+                raise calFileReadExcept, "<TEM> ID attribute value %d, expected (0 - 15)" % tem
+            towers.append(tem)
+
+        return towers            
+
+        
+    def read(self):
+        """
+        Read CAL DAC data from a ConfigSystem settings XML file
+
+        Returns: A Numeric array of data (16, 8, 2, 12) read from the
+        XML file.
+        """
+
+        # create empty DAC data array
+
+        dacData = Numeric.zeros((calConstant.NUM_TEM, calConstant.NUM_ROW, calConstant.NUM_END,
+                                 calConstant.NUM_FE), Numeric.UInt8)
+
+        # find <fle_dac> elements
+
+        doc = self.getDoc()
+
+        latList = doc.xpath('.//LATC_XML')
+        latLen = len(latList)
+        if latLen != 1:
+            raise calFileReadExcept, "found %d <LATC_XML> elements, expected 1" % latLen
+        temList = latList[0].xpath('.//TEM')
+        temLen = len(temList)
+        if temLen > 16:
+            raise calFileReadExcept, "found %d <TEM> elements, expected <= 16" % temLen
+        for t in temList:
+            tem = int(t.getAttributeNS(None, 'ID'))
+            if tem < 0 or tem > 16:
+                raise calFileReadExcept, "<TEM> ID attribute value %d, expected (0 - 15)" % tem
+            cccList = t.xpath('.//CCC')
+            cccLen = len(cccList)
+            if cccLen > 4:
+                raise calFileReadExcept, "found %d <CCC> elements, expected <= 4" % cccLen
+            for c in cccList:
+                ccc = int(c.getAttributeNS(None, 'ID'))
+                if ccc < 0 or ccc > 3:
+                    raise calFileReadExcept, "<CCC> ID attribute value %d, expected (0 - 3)" % ccc
+                rcList = c.xpath('.//CRC')
+                rcLen = len(rcList)
+                if rcLen > 4:
+                    raise calFileReadExcept, "found %d <CRC> elements, expected <= 4" % rcLen
+                for r in rcList:
+                    rc = int(r.getAttributeNS(None, 'ID'))
+                    if rc < 0 or rc > 3:
+                        raise calFileReadExcept, "<CRC> ID attribute value %d, expected (0 - 3)" % rc
+                    feList = r.xpath('.//CFE')
+                    feLen = len(feList)
+                    if feLen > 12:
+                        raise calFileReadExcept, "found %d <CFE> elements, expected <= 12" % feLen
+                    for f in feList:
+                        fe = int(f.getAttributeNS(None, 'ID'))
+                        if fe < 0 or fe > 11:
+                            raise calFileReadExcept, "<CFE> ID attribute value %d, expected (0 - 11)" % fe
+                        dacList = f.xpath('.//%s' % self.__dacName)
+                        dacLen = len(dacList)
+                        if dacLen != 1:
+                            if dacLen == 0:
+                                continue
+                            else:
+                                raise calFileReadExcept, "found %d %s elements, expected 1" % (dacLen, self.__dacName)
+                        d = dacList[0]
+                        dd = d.childNodes[0]
+                        dac = int(dd.data.strip(), 16)
+                        (row, end) = ccToRow(ccc, rc)
+                        dacData[tem, row, end, fe] = dac
+
+        return dacData
+        
+        
+        
+    def write(self, dacData, tems = (0,)):
+        """
+        Write CAL data to a ConfigSystem settings XML file
+
+        Param: dacData - A Numeric array of data (16, 8, 2, 12) to write to the
+        XML file.
+        Param: tems - A list of TEM ID values to include in the output data set.
+        """
+
+        doc = self.getDoc()
+        
+        # insert <LATC_XML> element  
+            
+        gl = doc.createElementNS(None, 'LATC_XML')
+        doc.appendChild(gl)     
+
+        for tem in tems:
+                        
+            # insert <TEM> elements
+
+            gt = doc.createElementNS(None, 'TEM')
+            gt.setAttributeNS(None, 'ID', str(tem))
+            gl.appendChild(gt)
+            
+            #c = doc.createComment("module = %s" % calConstant.CMOD[tem])
+            #gt.appendChild(c)
+            
+            for ccc in range(4):
+
+                # insert <CCC> elements
+
+                gc = doc.createElementNS(None, 'CCC')
+                gc.setAttributeNS(None, 'ID', str(ccc))
+                gt.appendChild(gc)                
+
+                for rc in range(4):
+
+                    # insert <CRC> elements
+
+                    gr = doc.createElementNS(None, 'CRC')
+                    gr.setAttributeNS(None, 'ID', str(rc))
+                    gc.appendChild(gr)
+                                    
+                    # translate index
+
+                    (row, end) = ccToRow(ccc, rc) 
+                    #c = doc.createComment("layer = %s%s " % (calConstant.CROW[row], calConstant.CPM[end]))
+                    #gr.appendChild(c) 
+                    
+                    for fe in range(12):
+
+                        # insert <CFE> elements
+
+                        gf = doc.createElementNS(None, 'CFE')
+                        gf.setAttributeNS(None, 'ID', str(fe))
+                        gr.appendChild(gf)
+
+                        # insert <xxx> elements
+
+                        dv = doc.createElementNS(None, self.__dacName)
+                        t = doc.createTextNode('0x%x' % int(dacData[tem, row, end, fe]))
+                        dv.appendChild(t)
+                        gf.appendChild(dv)
+                        
+        
+        # write output XML file
+
+        self.writeFile()
+        
+        
 
 
 def ccToRow(ccc, rc):
