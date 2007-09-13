4302880fcdf57255bdcad8b24e0380d4181e74ba
fewtrell <>
2007-09-13 19:21:29 +0000

    preparing for new tag

--- src/mainpage.h
+++ src/mainpage.h
@@ -1,5 +1,5 @@
 // (Special "header" just for doxygen)
-// $Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/src/mainpage.h,v 1.48 2007/02/27 20:44:12 fewtrell Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/src/mainpage.h,v 1.49 2007/04/10 14:51:01 fewtrell Exp $
 
 /*!
   @mainpage  package calibGenCAL
@@ -142,6 +142,11 @@
 
   This tool can be used as a pre-processor for other tools which require
   clean characterization tables to work well (e.g. genXXXsettings).
+
+  - <tt> dacBlockSet  [-t twr] [-l lyr] [-c col] [-f POS|NEG][-R pct] FLE|FHE|LAC|ULD <input_xml> <val_src> <output_xml> </tt>
+
+  <p> dacBlockSet script will overwrite a given dac settings file with single value or values from another input file.  It allows for 
+      several masking options which overwrite only a subset of the settings.  You may mask by tower, layer, column, face or random mask.
 
   <h5> calibGain scripts </h5>
   - <tt> calibGainCoeff </tt> : extract ratio of calibGain setting On/Off for HE
