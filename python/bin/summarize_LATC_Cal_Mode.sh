4c11a6201ddd62f42fa051b3f661d0148ad06355
fewtrell <>
2007-12-20 17:36:46 +0000

    launcher scripts added

---
+++ python/bin/summarize_LATC_Cal_Mode.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/bin/summarize_LATC_Cal_Mode.sh,v 1.1 2007/12/20 00:36:51 fewtrell Exp $
+export -n DISPLAY
+
+
+python ${CALIBGENCALROOT}/python/summarize_LATC_Cal_Mode.py "$@"D
