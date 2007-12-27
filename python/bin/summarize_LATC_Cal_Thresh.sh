9dc4df3ff011a1adf0b898274caed236d9233c17
fewtrell <>
2007-12-27 21:57:27 +0000

    fixed typo fixed typo

--- python/bin/summarize_LATC_Cal_Thresh.sh
+++ python/bin/summarize_LATC_Cal_Thresh.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/bin/summarize_LATC_Cal_Thresh.sh,v 1.2 2007/12/21 17:42:10 fewtrell Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/bin/summarize_LATC_Cal_Thresh.sh,v 1.1 2007/12/27 21:54:34 fewtrell Exp $
 export -n DISPLAY
 
 
-python ${CALIBGENCALROOT}/python/summarize_LATC_CAL_Thresh.py "$@"
+python ${CALIBGENCALROOT}/python/summarize_LATC_Cal_Thresh.py "$@"
