1f1317e91dde2d6bdf510b9c19128b4ee2c4fbed
fewtrell <>
2008-04-29 15:46:36 +0000

    adding tholdCIDiff application (not tested yet)

---
+++ python/bin/tholdCIDiff.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+#$Header: /nfs/slac/g/glast/ground/cvs/calibGenCAL/python/bin/tholdCIDiff.sh,v 1.1 2008/04/21 20:25:21 fewtrell Exp $
+export -n DISPLAY
+
+
+python ${CALIBGENCALROOT}/python/tholdCIDiff.py "$@"
+
