883f9ab234b80f67864ad4d923330d0f44efa455
fewtrell <>
2008-05-20 14:12:25 +0000

    added summary histograms

--- python/inlPlot.py
+++ python/inlPlot.py
@@ -13,8 +13,8 @@ where:
 __facility__  = "Offline"
 __abstract__  = "Create plots of Cal intNonlin curves"
 __author__    = "Z. Fewtrell"
-__date__      = "$Date: 2008/02/11 21:35:57 $"
-__version__   = "$Revision: 1.3 $, $Author: fewtrell $"
+__date__      = "$Date: 2008/04/21 14:36:57 $"
+__version__   = "$Revision: 1.1 $, $Author: fewtrell $"
 __release__   = "$Name:  $"
 __credits__   = "NRL code 7650"
 
@@ -54,7 +54,18 @@ if __name__ == '__main__':
     # create output file
     rootFile = ROOT.TFile(outName, "RECREATE")
 
+    # create summary plots for each ADC range
+    summaryHists = {}
+    for rng in range(calConstant.NUM_RNG):
+        plot_name = "inl_summary_" + calConstant.CRNG[rng]
+        summaryHists[rng] = ROOT.TH2S(plot_name,
+                                      plot_name,
+                                      4096,0,4096,
+                                      4096,0,4096)
+
+
     for twr in towers:
+        print "inlPLot.py processing TEM# " + str(twr)
         for offline_lyr in range(8):
             # calCalibXML uses 'row' indexing, not layer
             online_row = calCalibXML.layerToRow(offline_lyr)
@@ -63,10 +74,12 @@ if __name__ == '__main__':
                     online_face = calConstant.offline_face_to_online[offline_face]
                     for rng in range(4):
                         nPts = lenData[rng][twr,online_row,online_face, col,0]
+                        # hack: some xml files have splines w/ single point of (0,0) since dtd does not allow for missing data
+                        if nPts <= 1:
+                            continue
                         adcs = array.array('d',adcData[rng][twr,online_row,online_face, col])
                         dacs = array.array('d',dacData[rng][twr,online_row,online_face, col])
-                                           
-                        
+
                         # plot spline method
                         channel_str = "T%dL%dC%dF%dR%d"%(twr,offline_lyr,col,offline_face,rng)
                         spline = ROOT.TSpline3(channel_str, dacs, adcs, nPts)
@@ -85,6 +98,13 @@ if __name__ == '__main__':
                         # save plot to file
                         c.Write()
 
-                        
+                        # write points to summary histogram
+                        summaryHists[rng].FillN(len(adcs),
+                                                dacs,
+                                                adcs,
+                                                array.array('d',[1]*len(adcs)))
 
+    
+    for rng in range(calConstant.NUM_RNG):
+        summaryHists[rng].Write()
     rootFile.Close()
