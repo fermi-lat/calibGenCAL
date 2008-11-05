707f389a39eddd206faca6de19309d116871ca49
ecephas <>
2008-11-05 22:43:09 +0000

    Added the following libraries:      env.Tool('CalUtilLib')      env.Tool('calibUtilLib')      env.Tool('gcrSelectRootDataLib')      env.Tool('addLibrary', library = env['clhepLibs'])      env.Tool('reconRootDataLib')      env.Tool('digiRootDataLib')      env.Tool('mcRootDataLib')      env.Tool('addLibrary', library = env['pythonLibs'])

--- calibGenCALLib.py
+++ calibGenCALLib.py
@@ -1,12 +1,18 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/calibGenCAL/calibGenCALLib.py,v 1.1 2008/08/15 21:42:37 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['calibGenCAL'])
     env.Tool('addLibrary', library = env['rootLibs'])
     env.Tool('addLibrary', library = env['rootGuiLibs'])
     env.Tool('addLibrary', library = env['minuitLibs'])
-#If the Minuit library above causes any problems, try replacing the previous statement with env.Tool('minuitLibs') because it contains a Minuit2 library.
-
+    env.Tool('CalUtilLib')
+    env.Tool('calibUtilLib')
+    env.Tool('gcrSelectRootDataLib')
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('reconRootDataLib')
+    env.Tool('digiRootDataLib')
+    env.Tool('mcRootDataLib')
+    env.Tool('addLibrary', library = env['pythonLibs'])
 
 def exists(env):
     return 1;
